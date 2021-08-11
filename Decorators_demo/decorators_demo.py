6945f7b8e4a3fc2281c20362d788972ea89c8aca
Ephrem Wube <ephremworkeye@gmail.com>
2021-08-10 20:54:40 -0700

    simple decorator added

--- Decorators_demo/decorators_demo.py
+++ Decorators_demo/decorators_demo.py
@@ -4,44 +4,44 @@
         # Without explicitly modifying the funciton
 
 # simple decorator
-# def my_decorator(func):
-#     def wrapper(): #wrapping functon
-#         print('printed before')
-#         func()
-#         print('printed after')
-#         # print(wrapper)
-#     return wrapper  # return function for later use
+def my_decorator(func):
+    def wrapper(): #wrapping functon
+        print('printed before')
+        func()
+        print('printed after')
+        # print(wrapper)
+    return wrapper  # return function for later use
 
 
-# def another():
-#     print('Hi another')
+def another():
+    print('Hi another')
 
-# def added():
-#     print('Wow it is added')
+def added():
+    print('Wow it is added')
 
-# another = my_decorator(another)
-# added_func = my_decorator(added)
-# another()
-# added_func()
+another = my_decorator(another)
+added_func = my_decorator(added)
+another()
+added_func()
 
 # syntathic sugar
 
-# def my_decor(my_func):
-#     def wrapper():
-#         print('First print')
-#         my_func()
-#         print('Printed after "my_func" call')
-#     return wrapper
+def my_decor(my_func):
+    def wrapper():
+        print('First print')
+        my_func()
+        print('Printed after "my_func" call')
+    return wrapper
 
-# # instead of asigninig the returned function, we can use @parent funct
-# @my_decor # add this befroe defining the function
-# def func_one():
-#     print('func_one is printed')
+# instead of asigninig the returned function, we can use @parent funct
+@my_decor # add this befroe defining the function
+def func_one():
+    print('func_one is printed')
 
-# @my_decor
-# def func_two():
-#     print('func_two is printed')
+@my_decor
+def func_two():
+    print('func_two is printed')
 
 
-# func_one()
-# func_two()
+func_one()
+func_two()
