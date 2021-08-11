b8a0ee7443be7a3635d4f0fe569a5daebedeb9d9
Ephrem Wube <ephremworkeye@gmail.com>
2021-08-11 03:47:43 -0700

    debugging using decorators

--- Decorators_demo/decor_example.py
+++ Decorators_demo/decor_example.py
@@ -36,3 +36,19 @@ def timer(func):
         print(f'Finished {func.__name__!r} in {run_time:.4f} secs')
         return value
     return wrapper_timer
+
+
+def debug(func):
+    '''Print the function signature and return value'''
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}= {v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f'{func.__name__!r} returned {value!r}')
+        return value
+
+    return wrapper_debug
+
--- Decorators_demo/examples.py
+++ Decorators_demo/examples.py
@@ -6,3 +6,13 @@ def square_sum(num_times):
         sum([i**2 for i in range(10000)])
 
 square_sum(4)
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f"Hello {name}!"
+    else:
+        return f"Hello {name} you are already {age}"
+
+make_greeting('Sador')
+make_greeting('Sador', 4)
