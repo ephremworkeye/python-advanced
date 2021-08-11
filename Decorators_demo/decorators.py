3ce02ea27e567d77589a6937d5c186a3caeebd1b
Ephrem Wube <ephremworkeye@gmail.com>
2021-08-10 23:16:29 -0700

    decorated with required arguments, args and kwargs

--- Decorators_demo/decorators.py
+++ Decorators_demo/decorators.py
@@ -5,3 +5,19 @@ def say_hi(func):
         func()
     return wrapper
 
+#with arguments
+def say_something(func1):
+    def wrapper_arg(argument):
+        func1(argument)
+        func1(argument)
+    return wrapper_arg
+
+# using *args and **kwargs
+def say_selam(func3):
+    def wrapper2(*args, **kwargs):
+        func3(*args, **kwargs)
+        func3(*args, **kwargs)
+    return wrapper2
+
+
+
--- Decorators_demo/reuse.py
+++ Decorators_demo/reuse.py
@@ -1,8 +1,31 @@
-from decorators import say_hi
-
+from decorators import say_hi, say_something, say_selam
+#with out arguments
 @say_hi
 def greet():
     print('printed from decorators file')
 
 greet()
+
+
+
+# with arguments
+@say_something
+def greeting(name):
+    print(f'Hello {name}')
+
+greeting('Sador')
+
+#with args and kwargs
+@say_selam
+def selamta():
+    print('this is without arguments')
+
+selamta()
+
+#with arguments
+@say_selam
+def selmata2(name):
+    print(f'Selam {name}')
+
+selmata2('Demelash')
 
