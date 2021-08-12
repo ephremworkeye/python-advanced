e70eac7f38589a36a0edccb3f609547ab8b31931
Ephrem Wube <ephremworkeye@gmail.com>
2021-08-12 02:37:23 -0700

    Read data from tables

--- python_mysql/Mysql_test.py
+++ python_mysql/Mysql_test.py
@@ -134,28 +134,36 @@ try:
         #     cursor.executemany(insert_reviewers_query, reviewers_records)
         #     connection.commit()
 
-        insert_ratings_query = """
-        INSERT INTO ratings
-        (rating, movie_id, reviewer_id)
-        VALUES ( %s, %s, %s)
-        """
-        ratings_records = [
-            (6.4, 17, 5), (5.6, 19, 1), (6.3, 22, 14), (5.1, 21, 17),
-            (5.0, 5, 5), (6.5, 21, 5), (8.5, 30, 13), (9.7, 6, 4),
-            (8.5, 24, 12), (9.9, 14, 9), (8.7, 26, 14), (9.9, 6, 10),
-            (5.1, 30, 6), (5.4, 18, 16), (6.2, 6, 20), (7.3, 21, 19),
-            (8.1, 17, 18), (5.0, 7, 2), (9.8, 23, 3), (8.0, 22, 9),
-            (8.5, 11, 13), (5.0, 5, 11), (5.7, 8, 2), (7.6, 25, 19),
-            (5.2, 18, 15), (9.7, 13, 3), (5.8, 18, 8), (5.8, 30, 15),
-            (8.4, 21, 18), (6.2, 23, 16), (7.0, 10, 18), (9.5, 30, 20),
-            (8.9, 3, 19), (6.4, 12, 2), (7.8, 12, 22), (9.9, 15, 13),
-            (7.5, 20, 17), (9.0, 25, 6), (8.5, 23, 2), (5.3, 30, 17),
-            (6.4, 5, 10), (8.1, 5, 21), (5.7, 22, 1), (6.3, 28, 4),
-            (9.8, 13, 1)
-        ]
+        # insert_ratings_query = """
+        # INSERT INTO ratings
+        # (rating, movie_id, reviewer_id)
+        # VALUES ( %s, %s, %s)
+        # """
+        # ratings_records = [
+        #     (6.4, 17, 5), (5.6, 19, 1), (6.3, 22, 14), (5.1, 21, 17),
+        #     (5.0, 5, 5), (6.5, 21, 5), (8.5, 30, 13), (9.7, 6, 4),
+        #     (8.5, 24, 12), (9.9, 14, 9), (8.7, 26, 14), (9.9, 6, 10),
+        #     (5.1, 30, 6), (5.4, 18, 16), (6.2, 6, 20), (7.3, 21, 19),
+        #     (8.1, 17, 18), (5.0, 7, 2), (9.8, 23, 3), (8.0, 22, 9),
+        #     (8.5, 11, 13), (5.0, 5, 11), (5.7, 8, 2), (7.6, 25, 19),
+        #     (5.2, 18, 15), (9.7, 13, 3), (5.8, 18, 8), (5.8, 30, 15),
+        #     (8.4, 21, 18), (6.2, 23, 16), (7.0, 10, 18), (9.5, 30, 20),
+        #     (8.9, 3, 19), (6.4, 12, 2), (7.8, 12, 22), (9.9, 15, 13),
+        #     (7.5, 20, 17), (9.0, 25, 6), (8.5, 23, 2), (5.3, 30, 17),
+        #     (6.4, 5, 10), (8.1, 5, 21), (5.7, 22, 1), (6.3, 28, 4),
+        #     (9.8, 13, 1)
+        # ]
+        # with connection.cursor() as cursor:
+        #     cursor.executemany(insert_ratings_query, ratings_records)
+        #     connection.commit()
+
+        # # reading data from table
+        select_movies_query = "SELECT * FROM movies LIMIT 5"
         with connection.cursor() as cursor:
-            cursor.executemany(insert_ratings_query, ratings_records)
-            connection.commit()
+            cursor.execute(select_movies_query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
 
 except Error as e:
     print(e)
