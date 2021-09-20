9e9fdd073861a238c7bebf915890dfb1c442c4c5
Ephrem Wube <ephremworkeye@gmail.com>
2021-09-19 22:22:22 -0700

    string module added

---
+++ string_module/string_mod.py
@@ -0,0 +1,9 @@
+import string
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.hexdigits)
+print(string.digits)
+print(string.octdigits)
+print(string.octdigits)
+print(dir(string))
